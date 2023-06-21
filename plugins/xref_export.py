fd83fc78fc1268bd098db66ab2f1a87e424114a7
michal-kapala <mim4k1@gmail.com>
2023-06-21 02:31:27 +0200

    xref path export
    + upward xref paths added
    + bug fixes and stability improvements

--- plugins/xref_export.py
+++ plugins/xref_export.py
@@ -2,8 +2,17 @@ import idaapi, idc, sark
 import sqlite3
 import os, platform
 
-MAX_REFERENCE_DEPTH = 3
-"""Maximal evaluated distance in reference graph (or a stacktrace, measured in function calls)."""
+MAX_UP_REFERENCE_DEPTH = 1
+"""Maximal evaluated distance in upward traversal of reference graph (measured in function calls)."""
+
+MAX_DOWN_REFERENCE_DEPTH = 2
+"""Maximal evaluated distance in downward traversal of reference graph (measured in function calls)."""
+
+IDATA = sark.Segment(name=".idata")
+"""`.idata` imports section (Sark segment)."""
+
+RDATA = sark.Segment(name=".rdata")
+"""`.rdata` data section (Sark segment)."""
 
 def get_filename():
   """Returns the extensionless IDB file name."""
@@ -11,19 +20,27 @@ def get_filename():
   return os.path.splitext(os.path.basename(path))[0]
 
 def is_import(ea):
-  """Checks if an address belongs to `.idata`."""
-  idata = sark.Segment(name=".idata")
-  return ea >= idata.ea and ea < idata.ea + idata.size
+  """Checks if an address belongs to `.idata` section."""
+  return ea >= IDATA.ea and ea < IDATA.ea + IDATA.size
+
+def is_before_rdata(ea):
+  """Checks if the address is from before `.rdata` section."""
+  return ea < RDATA.ea
 
 def is_tostring_xref(xref):
   """Checks if cross reference points to a string."""
-  return xref.type.is_data and idc.GetType(xref.to) == "char[]"
+  # IDAPython docs say it returns empty string on failure but it actually returns None
+  # https://www.hex-rays.com/products/ida/support/idapython_docs/idc.html#idc.get_strlit_contents
+  return xref.type.is_data and idc.get_strlit_contents(xref.to) not in [None, ""] and not is_before_rdata(xref.to)
 
 def traverse_referrer_referee(cur, top_func, path_func1, path_func2, path_func3, ref_depth, is_upward):
   """Adds string xrefs from a referrer or referee function."""
-  # depth check
+  # depth checks
   ref_depth += 1
-  if ref_depth == MAX_REFERENCE_DEPTH:
+  if not is_upward and ref_depth > MAX_DOWN_REFERENCE_DEPTH:
+    return
+  
+  if is_upward and ref_depth > MAX_UP_REFERENCE_DEPTH:
     return
   
   # choose passed function
@@ -50,10 +67,8 @@ def traverse_referrer_referee(cur, top_func, path_func1, path_func2, path_func3,
     # function xref - traverse downward function xrefs
     if xref.type.is_code and not is_upward:
       if ref_depth == 1:
-        print("Passing xref for lvl 2 down traversal: " + str(func.ea) + " -> " + str(xref.to))
         traverse_referrer_referee(cur, top_func, path_func1, xref.to, path_func3, ref_depth, False)
       elif ref_depth == 2:
-        print("Passing xref for lvl 3 down traversal: " + str(func.ea) + " -> " + str(xref.to))
         traverse_referrer_referee(cur, top_func, path_func1, path_func2, xref.to, ref_depth, False)
       elif ref_depth == 3:
         # dont enter 4th depth level
@@ -65,7 +80,6 @@ def traverse_referrer_referee(cur, top_func, path_func1, path_func2, path_func3,
 
     # string xref - add x-level reference
     else:
-      print("Adding " + str(ref_depth) + "-level xref: " + str(func.ea) + " -> " + str(xref.to))
       try:
         cur.execute("INSERT INTO xrefs (func_addr,string_addr,path_func1,path_func2,path_func3,ref_depth,is_upward) VALUES (?,?,?,?,?,?,?)", (
           top_func, xref.to, path_func1, path_func2, path_func3, ref_depth, is_upward
@@ -74,23 +88,21 @@ def traverse_referrer_referee(cur, top_func, path_func1, path_func2, path_func3,
         pass
     
   # traverse upward function xrefs
-  if is_upward and ref_depth < MAX_REFERENCE_DEPTH:
+  if is_upward and ref_depth < MAX_UP_REFERENCE_DEPTH:
     for xref in func.xrefs_to:
       # functions can only be referenced by other functions
       if not xref.type.is_code:
         continue
-      # skip import xrefs
-      if is_import(xref.frm):
+      # skip xrefs from instruction blocks (unstructured functions)
+      if idc.get_func_name(xref.frm) == "":
         continue
       
       if ref_depth == 1:
-        print("Passing xref for lvl 2 up traversal: " + str(func.ea) + " -> " + str(xref.to))
         traverse_referrer_referee(cur, top_func, path_func1, xref.frm, path_func3, ref_depth, True)
       elif ref_depth == 2:
-        print("Passing xref for lvl 3 up traversal: " + str(func.ea) + " -> " + str(xref.to))
         traverse_referrer_referee(cur, top_func, path_func1, path_func2, xref.frm, ref_depth, True)
       else:
-        raise Exception("Unimplemented reference recursion depth for MAX_REF_DEPTH=" + str(MAX_REFERENCE_DEPTH))
+        raise Exception("Unimplemented reference recursion depth for MAX_REF_DEPTH=" + str(MAX_DOWN_REFERENCE_DEPTH))
 
 
 def export_xrefs():
@@ -130,7 +142,6 @@ def export_xrefs():
 
   for func in sark.functions():
     # level 0 - downward search
-    print("Function " + func.name)
     for xref in func.xrefs_from:
       # skip all xrefs pointing neither to a function nor a string
       if not is_tostring_xref(xref) and not xref.type.is_code:
@@ -142,14 +153,12 @@ def export_xrefs():
 
       # function xref - traverse reference graph neighbours
       if xref.type.is_code:
-        print("Passing xref for lvl 1 down traversal: " + str(func.ea) + " -> " + str(xref.to))
         # using -1 because SQLite treats NULL as a unique value
         # and as a consequence doesn't honor the multi-column UNIQUE constraint
         # https://stackoverflow.com/a/2701903/14807735
         traverse_referrer_referee(c, func.ea, xref.to, -1, -1, ref_depth, False)
       # string xref - add 0-level reference
       else:
-        print("Adding 0-level xref: " + str(func.ea) + " -> " + str(xref.to))
         try:
           c.execute("INSERT INTO xrefs (func_addr,string_addr,path_func1,path_func2,path_func3,ref_depth,is_upward) VALUES (?,?,?,?,?,?,?)", (
             func.ea, xref.to, -1, -1, -1, 0, False
@@ -159,30 +168,28 @@ def export_xrefs():
           pass
 
     # level 0 - upward search
-    # for xref in func.xrefs_to:
-    #   # functions can only be referenced by other functions
-    #   if not xref.type.is_code:
-    #     continue
-    #   # skip import xrefs (just in case)
-    #   if is_import(xref.frm):
-    #     continue
+    for xref in func.xrefs_to:
+      # functions can only be referenced by other functions
+      if not xref.type.is_code:
+        continue
+      # skip xrefs from instruction blocks (unstructured functions)
+      if idc.get_func_name(xref.frm) == "":
+        continue
 
-    #   # function xref - traverse reference graph neighbours
-    #   if xref.type.is_code:
-    #     print("Passing xref for lvl 1 up traversal: " + str(func.ea) + " -> " + str(xref.to))
-    #     traverse_referrer_referee(c, func.ea, xref.to, -1, -1, ref_depth, True)
+      # function xref - traverse reference graph neighbours
+      if xref.type.is_code:
+        traverse_referrer_referee(c, func.ea, xref.frm, -1, -1, ref_depth, True)
 
-
-  print("Exported " + str(exported) + " level 0 xrefs.")
+  print("Exported " + str(exported) + " zero-level xref paths.")
   conn.commit()
   conn.close()
 
 
 class XrefExporterPlugin(idaapi.plugin_t):
   flags = idaapi.PLUGIN_PROC
-  comment = "dubRE Xref Exporter"
-  help = "This plugin exports cross-references to SQLite database."
-  wanted_name = "dubRE XRef Exporter"
+  comment = "dubRE Xref Path Exporter"
+  help = "This plugin exports cross-reference paths to SQLite database."
+  wanted_name = "dubRE XRef Path Exporter"
   wanted_hotkey = "Shift+X"
 
   def init(self):
